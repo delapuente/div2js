626d716d277c534036532375781e75bbce761d55
Salvador de la Puente <salva@unoyunodiez.com>
2016-03-25 16:41:13 +0100

    Adding escodegen 1.8.1 as generator. Adaptation to requirejs made manually.

--- src/compiler.js
+++ src/compiler.js
@@ -1,5 +1,19 @@
 
-define([], function () {
+define([
+  'div2lang',
+  'div2trans',
+  'lib/escodegen'
+], function (parser, translator, generator) {
   'use strict';
-  return {};
+
+  function compile(srcText) {
+    var div2Ast = parser.parse(srcText);
+    var jsAst = translator.translate(div2Ast);
+    var objText = generator.generate(jsAst);
+    return Promise.resolve(objText);
+  }
+
+  return {
+    compile: compile
+  };
 });
