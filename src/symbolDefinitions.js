421be6e04074803b391c63ac52f9f9be4d845c12
Salvador de la Puente González <salva@unoyunodiez.com>
2017-02-02 07:46:07 +0100

    WIP Symbol definitions

---
+++ src/symbolDefinitions.js
@@ -0,0 +1,270 @@
+define([], function () {
+  'use strict';
+
+  // The definition format of the memory map is described here.
+  //
+  // In DIV, memory is a continuous and int (4 bytes) directionable only array
+  // of cells. Pointer arithmetic can not address sub-int-size (word or byte)
+  // cells. DIV memory is 4 bytes aligned.
+  //
+  // Symbols is not a 1:1 memory map but a summary from which the memory map
+  // is derived. The main goal for symbol tables is to specify the relative
+  // order for specific memory regions.
+  //
+  // Each cell is defined by an object with the following fields:
+  //
+  //   * type   - is the type of the cell: byte (1 byte), word (2 bytes), int
+  //              (4 bytes, default if omitted) and struct.
+  //   * name   - is the name of the symbol.
+  //   * fields - if the type is struct, this is the list of symbols of the
+  //              struct.
+  //   * length - times to repeat this symbol (1 if omitted)
+  //
+  // Since most of the times, a symbol is 1 int, instead of an object, you
+  // can specify a string with the name of the symbol.
+
+  // Based on docs, sources and experimental tests:
+  // https://github.com/DIVGAMES/DIV-Games-Studio/blob/0c006cca548f9d6dc66d174d4f05d167148c7e78/dll/div.h
+  // Experimental tests are besed on measuring offsets between pairs of
+  // variables or struct fields. It seems, there are "hidden" variables.
+  //
+  // IMPORTANT!!
+  // Ultimate values are put to preserve experimental offsets. When multiple
+  // values preserve the offsets, I chose the one closest to what is
+  // documented.
+
+  return {
+    "wellKnownGlobals": [
+      {
+        "type": "struct",
+        "name": "mouse",
+        "fields": [
+          "x",
+          "y",
+          "z",
+          "file",
+          "graph",
+          "angle",
+          "size",
+          "flags",
+          "region",
+          "left",
+          "middle",
+          "right",
+          "cursor",
+          "speed"
+        ]
+      },
+      {
+        "type": "struct",
+        "name": "scroll",
+        "fields": [
+          "z",
+          "camera",
+          "ratio",
+          "speed",
+          "region1",
+          "region2",
+          "x0", "y0",
+          "x1", "y1"
+        ],
+        "length": 10
+      },
+      {
+        "type": "struct",
+        "name": "m7",
+        "fields": [
+          "z",
+          "camera",
+          "height",
+          "distance",
+          "horizon",
+          "focus",
+          "color"
+        ],
+        "length": 10
+      },
+      {
+        "type": "struct",
+        "name": "joy",
+        "fields": [
+          "button1",
+          "button2",
+          "button3",
+          "button4",
+          "left",
+          "right",
+          "up",
+          "down"
+        ]
+      },
+      {
+        "type": "struct",
+        "name": "setup",
+        "fields": [
+          "card",
+          "port",
+          "irq",
+          "dma",
+          "dma2",
+          "master",
+          "sound_fx",
+          "cd_audio",
+          "mixer",
+          "rate",
+          "bits"
+        ]
+      },
+      {
+        "type": "struct",
+        "name": "net",
+        "fields": [
+          "device",
+          "com",
+          "speed",
+          "number",
+          "init",
+          "mode",
+          "server",
+          "max_players",
+          "num_players"
+        ]
+      },
+      {
+        "type": "struct",
+        "name": "m8",
+        "fields": [
+          "z",
+          "camera",
+          "height",
+          "angle"
+        ],
+        "length": 10
+      },
+      {
+        "type": "struct",
+        "name": "dirinfo",
+        "fields": [
+          "files",
+          { "type": "int", "length": 1025, "name": "name" }
+        ]
+      },
+      {
+        "type": "struct",
+        "name": "fileinfo",
+        "fields": [
+          // No idea why these *_fix are here:
+          // https://github.com/DIVGAMES/DIV-Games-Studio/blob/0c006cca548f9d6dc66d174d4f05d167148c7e78/dll/div.h#L187
+          { "type": "int", "name": "fullpath_fix", "hidden": true },
+          { "type": "byte", "length": 256, "name": "fullpath" },
+          "drive",
+          { "type": "int", "name": "length_fix", "hidden": true },
+          { "type": "byte", "length": 256, "name": "dir" },
+          { "type": "int", "name": "name_fix", "hidden": true },
+          { "type": "byte", "length": 12, "name": "name" },
+          { "type": "int", "name": "ext_fix", "hidden": true },
+          { "type": "byte", "length": 8, "name": "ext" },
+          "size",
+          "day",
+          "month",
+          "year",
+          "hour",
+          "min",
+          "sec",
+          "attrib"
+        ]
+      },
+      {
+        "type": "struct",
+        "name": "video_modes",
+        "fields": [
+          "wide",
+          "height",
+          "mode"
+        ],
+        "length": 32
+      },
+      {
+        "name": "timer",
+        "length": 10
+      },
+      "text_z",
+      "fading",
+      "shift_status",
+      "ascii",
+      "scan_code",
+      "joy_filter",
+      "joy_status",
+      "restore_type",
+      "dump_type",
+      "max_process_time",
+      "fps",
+      "argc",
+      {
+        "name": "argv",
+        "length": 10
+      },
+      {
+        "name": "channel",
+        "length": 32
+      },
+      "vsync",
+      "draw_z",
+      "num_video_modes",
+      "unit_size"
+    ],
+
+    "wellKnownLocals": [
+      {
+        "type": "struct",
+        "name": "reserved",
+        "fields": [
+          "process_id",
+          "id_scan",
+          "process_type",
+          "type_scan",
+          "status",
+          "parameters",
+          "param_offset",
+          "program_index",
+          "stack_pointer",
+          "is_executed",
+          "is_painted",
+          "m8_object",
+          "old_ctype",
+          "frame_percent",
+          "box_x0",
+          "box_y0",
+          "box_x1",
+          "box_y1",
+          "f_count",
+          "caller_id"
+        ]
+      },
+      "father",
+      "son",
+      "smallbro",
+      "bigbro",
+      "priority",
+      "ctype",
+      "x",
+      "y",
+      "z",
+      "graph",
+      "flags",
+      "size",
+      "angle",
+      "region",
+      "file",
+      "xgraph",
+      "cnumber",
+      "height",
+      "resolution",
+      "radius",
+      "m8_wall",
+      "m8_sector",
+      "m8_nextsector",
+      "m8_step"
+    ]
+  };
+});
