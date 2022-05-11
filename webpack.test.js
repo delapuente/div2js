fd8ea63794aa5b010b1336398113181be14f5000
delapuente <salva@unoyunodiez.com>
2022-05-10 21:29:29 -0400

    Lint Webpack config missing file

--- webpack.test.js
+++ webpack.test.js
@@ -1,21 +1,23 @@
-const path = require('path');
-const base = require('./webpack.config');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const base = require("./webpack.config");
+/* eslint-enable @typescript-eslint/no-var-requires */
 
 delete base.entry;
 delete base.output;
 
 // Needed by Karma
-base.devtool = 'eval-source-map';
+base.devtool = "eval-source-map";
 
 base.module.rules.push({
   test: /\.(ts|js)$/,
   use: {
-    loader: 'istanbul-instrumenter-loader',
-    options: { esModules: true }
+    loader: "istanbul-instrumenter-loader",
+    options: { esModules: true },
   },
-  include: path.resolve('src/'),
-  enforce: 'post',
-  exclude: /node_modules|\.spec\.js$|div2lang\.js$/
+  include: path.resolve("src/"),
+  enforce: "post",
+  exclude: /node_modules|\.spec\.js$|div2lang\.js$/,
 });
 
 module.exports = base;
