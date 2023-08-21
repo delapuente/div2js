de82f9f88d070a348cc1847c56e250dac2cfce19
Salvador de la Puente Gonzalez <neo.salvador@gmail.com>
2023-08-20 23:25:05 -0400

    Display source code of the demo.

--- demos/demos.js
+++ demos/demos.js
@@ -1,33 +1,35 @@
-var programSelector = document.querySelector('select');
-var reloadButton = document.querySelector('button');
+const programSelector = document.querySelector("select");
+const reloadButton = document.querySelector("button");
+const demoSource = document.querySelector("#demo-source");
 
 reloadButton.onclick = load;
 programSelector.onchange = load;
 
-function load(evt) {
+function load() {
   reloadButton.disabled = true;
 
-  var programUrl = document.querySelector('#demo-list').value;
+  const programUrl = document.querySelector("#demo-list").value;
   if (!programUrl) {
     reloadButton.disabled = false;
     return;
   }
 
   fetch(programUrl)
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (source) {
-    return div2.compile(source);
-  })
-  .then(function (binary) {
-    return div2.load(binary);
-  })
-  .then(function (program) {
-    reloadButton.disabled = false;
-    program.run();
-  })
-  .catch(function (error) {
-    console.log(error.message);
-  });
-};
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (source) {
+      demoSource.innerHTML = source;
+      return div2.compile(source);
+    })
+    .then(function (binary) {
+      return div2.load(binary);
+    })
+    .then(function (program) {
+      reloadButton.disabled = false;
+      program.run();
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
+}
