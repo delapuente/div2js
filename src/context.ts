99069685eff53e8e0bad09cc1bdf2fef34e0b977
Salvador de la Puente Gonzalez <neo.salvador@gmail.com>
2024-08-05 21:17:09 -0400

    Conver Context, Linarization, and Label classes to ES6 classes.

--- src/context.ts
+++ src/context.ts
@@ -1,75 +1,80 @@
 import * as ast from "./ast";
 import t from "./templates";
 
-function Context(symbolTable, memoryMap) {
-  this._processes = {};
-  this._auxNames = {};
-  this._currentProcess = undefined;
-  this._symbolTable = symbolTable;
-  this._mmap = memoryMap;
-}
+class Context {
+  private _processes: {};
+  private _auxNames: {};
+  private _currentProcess: any;
+  private _symbolTable: any;
+  private _mmap: any;
+  private _currentLinearization: any;
 
-Context.prototype = {
-  constructor: Context,
+  constructor(symbolTable, memoryMap) {
+    this._processes = {};
+    this._auxNames = {};
+    this._currentProcess = undefined;
+    this._symbolTable = symbolTable;
+    this._mmap = memoryMap;
+  }
 
-  getMemoryMap: function () {
+  getMemoryMap() {
     return this._mmap;
-  },
+  }
 
-  startLinearization: function () {
+  startLinearization() {
     this._auxNames = {};
     this._currentLinearization = new Linearization();
-  },
+  }
 
-  getLinearizationCases: function () {
+  getLinearizationCases() {
     return this._currentLinearization.getCases();
-  },
+  }
 
-  end: function () {
+  end() {
     return this._currentLinearization.end();
-  },
+  }
 
-  callFunction: function (resumeLabel, name, argList) {
+  callFunction(resumeLabel, name, argList) {
     return this._currentLinearization.callFunction(resumeLabel, name, argList);
-  },
+  }
 
-  newProcess: function (resumeLabel, name, argList) {
+  newProcess(resumeLabel, name, argList) {
     return this._currentLinearization.newProcess(resumeLabel, name, argList);
-  },
+  }
 
-  clone: function (childLabel, parentLabel) {
+  clone(childLabel, parentLabel) {
     return this._currentLinearization.clone(childLabel, parentLabel);
-  },
+  }
 
-  frame: function (resumeLabel, expression) {
+  frame(resumeLabel, expression) {
     return this._currentLinearization.frame(resumeLabel, expression);
-  },
+  }
 
-  debug: function (resumeLabel) {
+  debug(resumeLabel) {
     return this._currentLinearization.debug(resumeLabel);
-  },
+  }
 
-  declareProcess: function (name) {
+  declareProcess(name) {
     this._processes[name] = true;
-  },
+  }
 
-  isProcess: function (name) {
+  isProcess(name) {
     return name in this._processes;
-  },
+  }
 
-  enterProcess: function (name) {
+  enterProcess(name) {
     this._currentProcess = name;
-  },
+  }
 
-  exitProcess: function () {
+  exitProcess() {
     this._currentProcess = undefined;
-  },
+  }
 
-  return: function (expression) {
+  return(expression) {
     return this._currentLinearization.return(expression);
-  },
+  }
 
-  newAux: function (name, initializer) {
+  newAux(name, initializer) {
     const nameCount = this._auxNames[name] || 0;
     const suffix = (this._auxNames[name] = nameCount + 1);
     if (nameCount > 0) {
@@ -83,37 +88,37 @@ Context.prototype = {
       identifier: identifier,
       declaration: declaration,
     };
-  },
+  }
 
-  newLabel: function () {
+  newLabel() {
     return this._currentLinearization.newLabel();
-  },
+  }
 
-  label: function (label) {
+  label(label) {
     return this._currentLinearization.label(label);
-  },
+  }
 
-  verbatim: function (ast) {
+  verbatim(ast) {
     return this._currentLinearization.verbatim(ast);
-  },
+  }
 
-  goToIf: function (testAst, labelIfTrue, labelIfFalse) {
+  goToIf(testAst, labelIfTrue, labelIfFalse) {
     return this._currentLinearization.goToIf(
       testAst,
       labelIfTrue,
       labelIfFalse
     );
-  },
+  }
 
-  goTo: function (label) {
+  goTo(label) {
     return this._currentLinearization.goTo(label);
-  },
+  }
 
-  select: function (evaluation, options, defaultLabel) {
+  select(evaluation, options, defaultLabel) {
     return this._currentLinearization.select(evaluation, options, defaultLabel);
-  },
+  }
 
-  getScope: function (identifier) {
+  getScope(identifier) {
     let scope;
     const symbols = this._symbolTable;
     if (symbols.isGlobal(identifier)) {
@@ -131,25 +136,26 @@ Context.prototype = {
       scope = "constant";
     }
     return scope;
-  },
+  }
 
-  constantValue: function (identifier) {
+  constantValue(identifier) {
     return this._symbolTable
       .constants
       .find((constant) => constant.name === identifier)
       .default;
   }
-};
-
-function Linearization() {
-  this._pc = -1;
-  this._sentences = [];
 }
 
-Linearization.prototype = {
-  constructor: Linearization,
+class Linearization {
+  private _pc: number;
+  private _sentences: any[];
 
-  getCases: function () {
+  constructor() {
+    this._pc = -1;
+    this._sentences = [];
+  }
+
+  getCases() {
     const cases = [];
     const sentences = this._sentences;
     let currentCase = null;
@@ -177,13 +183,13 @@ Linearization.prototype = {
       caseIsFinished = isReturn || (caseIsFinished && !isLabel);
     }
     return cases;
-  },
+  }
 
-  newLabel: function () {
+  newLabel() {
     return new Label();
-  },
+  }
 
-  label: function (label) {
+  label(label) {
     const lastSentence = this._sentences[this._sentences.length - 1];
     if (lastSentence instanceof Label) {
       label.proxy(lastSentence);
@@ -191,60 +197,60 @@ Linearization.prototype = {
       label.label = this._pc + 1;
       this._sentences.push(label);
     }
-  },
+  }
 
-  verbatim: function (sentence) {
+  verbatim(sentence) {
     this._addSentence(this._verbatim(sentence));
-  },
+  }
 
-  goToIf: function (testAst, labelIfTrue, labelIfFalse) {
+  goToIf(testAst, labelIfTrue, labelIfFalse) {
     this._addSentence(this._goToIf(testAst, labelIfTrue, labelIfFalse));
-  },
+  }
 
-  goTo: function (label) {
+  goTo(label) {
     this._addSentence(this._goTo(label));
-  },
+  }
 
-  select: function (evaluation, options, defaultLabel) {
+  select(evaluation, options, defaultLabel) {
     this._addSentence(this._select(evaluation, options, defaultLabel));
-  },
+  }
 
-  end: function () {
+  end() {
     this._addSentence(this._end());
-  },
+  }
 
-  callFunction: function (resumeLabel, name, argList) {
+  callFunction(resumeLabel, name, argList) {
     this._addSentence(this._call("function", resumeLabel, name, argList));
-  },
+  }
 
-  newProcess: function (resumeLabel, name, argList) {
+  newProcess(resumeLabel, name, argList) {
     this._addSentence(this._call("process", resumeLabel, name, argList));
-  },
+  }
 
-  clone: function (childLabel, parentLabel) {
+  clone(childLabel, parentLabel) {
     this._addSentence(this._clone(childLabel, parentLabel));
-  },
+  }
 
-  frame: function (resumeLabel, expression) {
+  frame(resumeLabel, expression) {
     this._addSentence(this._frame(resumeLabel, expression));
-  },
+  }
 
-  debug: function (resumeLabel) {
+  debug(resumeLabel) {
     this._addSentence(this._debug(resumeLabel));
-  },
+  }
 
-  return: function (expression) {
+  return(expression) {
     this._addSentence(this._return(expression));
-  },
+  }
 
-  _verbatim: function (sentence) {
+  _verbatim(sentence) {
     return {
       type: "Verbatim",
       sentences: [sentence],
     };
-  },
+  }
 
-  _goToIf: function (testAst, labelIfTrue, labelIfFalse) {
+  _goToIf(testAst, labelIfTrue, labelIfFalse) {
     const _this = this;
     return {
       type: "GoToIf",
@@ -257,11 +263,11 @@ Linearization.prototype = {
           ),
           new ast.BreakStatement(),
         ];
-      },
+      }
     };
-  },
+  }
 
-  _goTo: function (label) {
+  _goTo(label) {
     const _this = this;
     return {
       type: "GoTo",
@@ -270,11 +276,11 @@ Linearization.prototype = {
           _this._programCounterSet(label.label),
           new ast.BreakStatement(),
         ];
-      },
+      }
     };
-  },
+  }
 
-  _select: function (evaluation, options, defaultLabel) {
+  _select(evaluation, options, defaultLabel) {
     const _this = this;
     return {
       type: "Select",
@@ -290,66 +296,66 @@ Linearization.prototype = {
         return [defaultExpression]
           .concat(cases)
           .concat([new ast.BreakStatement()]);
-      },
+      }
     };
-  },
+  }
 
-  _end: function () {
-    return {
+  _end() {
+   return {
       type: "End",
       get sentences() {
         return [t.processEnd];
-      },
+      }
     };
-  },
+  }
 
-  _call: function (kind, resumeLabel, name, argList) {
+  _call(kind, resumeLabel, name, argList) {
     const type = { function: "CallFunction", process: "NewProcess" }[kind];
     return {
       type: type,
       get sentences() {
         return [t.call(kind, resumeLabel.label + 1, name, argList)];
-      },
+      }
     };
-  },
+  }
 
-  _clone: function (childLabel, parentLabel) {
+  _clone(childLabel, parentLabel) {
     return {
       type: "Clone",
       get sentences() {
         return [t.processClone(childLabel.label + 1, parentLabel.label + 1)];
-      },
+      }
     };
-  },
+  }
 
-  _frame: function (resumeLabel, expression) {
+  _frame(resumeLabel, expression) {
     return {
       type: "Frame",
       get sentences() {
         return [t.processFrame(resumeLabel.label + 1, expression)];
-      },
+      }
     };
-  },
+  }
 
-  _debug: function (resumeLabel) {
+  _debug(resumeLabel) {
     return {
       type: "Debug",
       get sentences() {
         return [t.processDebug(resumeLabel.label + 1)];
-      },
+      }
     };
-  },
+  }
 
-  _return: function (expression) {
+  _return(expression) {
     return {
       type: "Return",
       get sentences() {
         return [t.processReturn(expression)];
-      },
+      }
     };
-  },
+  }
 
-  _programCounterBranch: function (testAst, consequent, alternate) {
+  _programCounterBranch(testAst, consequent, alternate?) {
     return new ast.ExpressionStatement(
       new ast.AssignmentExpression(
         t.programCounter,
@@ -360,42 +366,43 @@ Linearization.prototype = {
         )
       )
     );
-  },
+  }
 
-  _programCounterSet: function (label) {
+  _programCounterSet(label) {
     return new ast.ExpressionStatement(
       new ast.AssignmentExpression(t.programCounter, new ast.Literal(label + 1))
     );
-  },
+  }
 
-  _addSentence: function (ast) {
+  _addSentence(ast) {
     if (!this._sentences.length) {
       this._sentences.push(new Label(0));
     }
     this._sentences.push(ast);
     this._pc += 1;
-  },
+  }
 };
 
-function Label(n?) {
-  this.label = n;
-}
+class Label {
+  private _proxifiedLabel: any;
+  label: any;
 
-Label.prototype = {
-  constructor: Label,
-
-  proxy: function (anotherLabel) {
+  constructor(n?) {
+    this.label = n;
+  }
+  
+  proxy(anotherLabel) {
     this._proxifiedLabel = anotherLabel;
     Object.defineProperty(this, "label", {
-      get: function () {
+     get() {
         return this._proxifiedLabel.label;
       },
     });
-  },
+  }
 
   get sentences() {
     return [];
-  },
-};
+  }
+}
 
 export { Context, Linearization };
