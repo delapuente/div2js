9ee54a6e3590a4f4d8eaf248d78b0e6b7565087b
Salvador de la Puente Gonzalez <neo.salvador@gmail.com>
2024-08-17 11:32:08 -0400

    Add scaling capability to xput(). Add blending semantics for pixels.

--- src/systems/video/wgl2idx/indexedGraphic.ts
+++ src/systems/video/wgl2idx/indexedGraphic.ts
@@ -8,6 +8,13 @@ class IndexedGraphic {
     this.buffer = new Uint8Array(width * height);
   }
 
+  // XXX: Captures the semantics of putting a pixel with some transformation regarding the color behind.
+  blendPixel(x: number, y: number, color: number, transparentColor: number) {
+    if (color !== transparentColor) {
+      this.putPixel(x, y, color);
+    }
+  }
+
   putPixel(x: number, y: number, color: number) {
     this.buffer[y * this.width + x] = color;
   }
@@ -42,24 +49,24 @@ class IndexedGraphic {
     region: number
   ) {
     // TODO: Rotation.
-    // TODO: Scaling.
     // TODO: Flags: flip.
     // TODO: Flags: transparent.
     // TODO: Flags: mirror.
     // TODO: Region.
     const { width: screenWidth, height: screenHeight } = this;
-    const xOffset = x - centerX;
-    const yOffset = y - centerY;
+    const scaleFactor = size / 100;
+    const scaledWidth = Math.floor(width * scaleFactor);
+    const scaledHeight = Math.floor(height * scaleFactor);
+    const xOffset = x - Math.floor(centerX * scaleFactor);
+    const yOffset = y - Math.floor(centerY * scaleFactor);
     const xStart = Math.max(0, xOffset);
     const yStart = Math.max(0, yOffset);
-    const xEnd = Math.min(screenWidth, xOffset + width);
-    const yEnd = Math.min(screenHeight, yOffset + height);
+    const xEnd = Math.min(screenWidth, xOffset + scaledWidth);
+    const yEnd = Math.min(screenHeight, yOffset + scaledHeight);
     for (let yScreen = yStart; yScreen < yEnd; yScreen += 1) {
       for (let xScreen = xStart; xScreen < xEnd; xScreen += 1) {
-        const color = data[(yScreen - yOffset) * width + (xScreen - xOffset)];
-        if (color !== 0) {
-          this.putPixel(xScreen, yScreen, color);
-        }
+        const color = data[Math.floor((yScreen - yOffset) / scaleFactor) * width + Math.floor((xScreen - xOffset) / scaleFactor)];
+        this.blendPixel(xScreen, yScreen, color, 0);        
       }
     }   
   }
