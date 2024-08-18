c4b59a6a7f4b477c3d3cf7f060d6dbb5f082b442
Salvador de la Puente Gonzalez <neo.salvador@gmail.com>
2024-08-18 17:59:58 -0400

    Add functionality to modify programs in the demo sandbox, directly.

--- demos/demos.js
+++ demos/demos.js
@@ -1,16 +1,24 @@
 const programSelector = document.querySelector("select");
 const reloadButton = document.querySelector("button");
 const demoSource = document.querySelector("#demo-source");
+const runButton = document.querySelector("#run");
+const shortcutsTip = document.querySelector("#shortcuts");
 
+runButton.onclick = run;
 reloadButton.onclick = load;
 programSelector.onchange = load;
+demoSource.onkeypress = (event) => {
+  if (event.ctrlKey && event.key === "Enter") {
+    run();
+  }
+};
 
 function load() {
   reloadButton.disabled = true;
+  runButton.disabled = true;
 
   const programUrl = document.querySelector("#demo-list").value;
   if (!programUrl) {
-    reloadButton.disabled = false;
     return;
   }
 
@@ -19,17 +27,21 @@ function load() {
       return response.text();
     })
     .then(function (source) {
-      demoSource.innerHTML = source;
-      return div2.compile(source);
-    })
-    .then(function (binary) {
-      return div2.load(binary);
-    })
-    .then(function (program) {
+      demoSource.value = source;
+      run();
+    });
+}
+
+function run() {
+  const source = demoSource.value;
+  const binary = div2.compile(source);
+  div2
+    .load(binary)
+    .then((program) => {
       reloadButton.disabled = false;
+      runButton.disabled = false;
+      shortcutsTip.removeAttribute("hidden");
       program.start();
     })
-    .catch(function (error) {
-      console.log(error.message);
-    });
+    .catch((error) => console.log(error.message));
 }
--- demos/prg/xput_example.prg
+++ demos/prg/xput_example.prg
@@ -11,14 +11,14 @@ PRIVATE
 BEGIN
   file1 = load_fpg("help/help.fpg");
   LOOP
-    x_coord = rand(0, 319);
-    y_coord = rand(0, 199);
-    angle1 = rand(-pi, pi);
-    size1 = rand(10, 200);
-    flags1 = rand(0, 7);
+    x_coord = 160;//rand(0, 319);
+    y_coord = 100;//rand(0, 199);
+    angle1 = 0;//rand(-pi, pi);
+    size1 = 100;//rand(10, 200);
+    flags1 = 0;//rand(0, 7);
 
     // Put graphic 101
-    xput(file1, 101, x_coord, y_coord, angle1, size1, flags1, 0);
+    xput(file1, 100, x_coord, y_coord, angle1, size1, flags1, 0);
 
     FRAME;
   END
