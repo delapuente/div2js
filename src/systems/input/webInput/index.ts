a59962233a6f9834bd7a5b2a23c33f05bad543b9
Salvador de la Puente Gonzalez <neo.salvador@gmail.com>
2024-12-21 13:07:04 -0500

    Identify mouse events.
    I'm thinking I should add more control to system running. Running
    systems is today done after executing the process and I'm thinking
    if some things should happend before and other after... Dunno.

--- src/systems/input/webInput/index.ts
+++ src/systems/input/webInput/index.ts
@@ -4,6 +4,9 @@ import { Div2InputSystem } from "../div2InputSystem";
 export default class WebInputSystem implements Div2InputSystem, System {
   private _mouseX: number = 0;
   private _mouseY: number = 0;
+  private _left: boolean = false;
+  private _right: boolean = false;
+  private _middle: boolean = false;
 
   constructor(
     private readonly _width: number,
@@ -12,9 +15,17 @@ export default class WebInputSystem implements Div2InputSystem, System {
   ) {}
 
   initialize() {
-    // Liste to mouse move events.
+    // Listen to mouse move events.
     this._canvas.addEventListener("mousemove", (event) => {
       this._onMouseMove(event.offsetX, event.offsetY);
+    });
+    // Listen to mouse down events.
+    this._canvas.addEventListener("mousedown", (event) => {
+      this._onMouseDown(event.button);
+    });
+    // Listen to mouse up events.
+    this._canvas.addEventListener("mouseup", (event) => {
+      this._onMouseUp(event.button);
     });
   }
 
@@ -23,8 +34,43 @@ export default class WebInputSystem implements Div2InputSystem, System {
     this._mouseY = Math.floor((y / this._canvas.height) * this._height);
   }
 
+  private _onMouseDown(button: number) {
+    switch (button) {
+      case 0:
+        this._left = true;
+        break;
+      case 1:
+        this._middle = true;
+        break;
+      case 2:
+        this._right = true;
+        break;
+    }
+  }
+
+  private _onMouseUp(button: number) {
+    switch (button) {
+      case 0:
+        this._left = false;
+        break;
+      case 1:
+        this._middle = false;
+        break;
+      case 2:
+        this._right = false;
+        break;
+    }
+  }
+
   run(runtime: Runtime) {
     runtime.getMemoryBrowser().global("mouse.x").value = this._mouseX;
     runtime.getMemoryBrowser().global("mouse.y").value = this._mouseY;
+    runtime.getMemoryBrowser().global("mouse.left").value = this._left ? 1 : 0;
+    runtime.getMemoryBrowser().global("mouse.middle").value = this._middle
+      ? 1
+      : 0;
+    runtime.getMemoryBrowser().global("mouse.right").value = this._right
+      ? 1
+      : 0;
   }
 }
